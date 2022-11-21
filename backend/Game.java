4942746c51dbcb558d7b2896660e99cac42fcc9f
colemccauley00 <colemccauley00@gmail.com>
2022-11-20 19:40:17 -0600

    hashmap for pieces
    removed old classes and fixed small bugs.

--- backend/Game.java
+++ backend/Game.java
@@ -1,43 +1,48 @@
 package backend;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
 public class Game {
 	private PieceData[][] board = new PieceData[8][8];
-	private PieceData[] pieces = new PieceData[32];
+	private HashMap<List<Integer>, PieceData> pieces = new HashMap<List<Integer>,PieceData>();
 	
 	public void initializeBoard() {
-		pieces[0] = new PieceData("b","rook",0,0);
-		pieces[1] = new PieceData("b","knight",1,0);
-		pieces[2] = new PieceData("b","bishop",2,0);
-		pieces[3] = new PieceData("b","queen",3,0);
-		pieces[4] = new PieceData("b","king",4,0);
-		pieces[5] = new PieceData("b","bishop",5,0);
-		pieces[6] = new PieceData("b","knight",6,0);
-		pieces[7] = new PieceData("b","rook",7,0);
+		pieces.put(Arrays.asList(0,0), new PieceData("b","rook",0,0));
+		pieces.put(Arrays.asList(1,0), new PieceData("b","knight",1,0));
+		pieces.put(Arrays.asList(2,0), new PieceData("b","bishop",2,0));
+		pieces.put(Arrays.asList(3,0), new PieceData("b","queen",3,0));
+		pieces.put(Arrays.asList(4,0), new PieceData("b","king",4,0));
+		pieces.put(Arrays.asList(5,0), new PieceData("b","bishop",5,0));
+		pieces.put(Arrays.asList(6,0), new PieceData("b","knight",6,0));
+		pieces.put(Arrays.asList(7,0), new PieceData("b","rook",7,0));
 		for (int x = 0; x<board.length;x++) {
-			pieces[8+x] = new PieceData("b","pawn",x,1);
+			pieces.put(Arrays.asList(x,1), new PieceData("b","pawn",x,1));
 		}
 		for (int x = 0; x<board.length;x++) {
-			pieces[16+x] = new PieceData("w","pawn",x,6);
+			pieces.put(Arrays.asList(x,6), new PieceData("w","pawn",x,6));
 		}
-		pieces[24] = new PieceData("w","rook",0,7);
-		pieces[25] = new PieceData("w","knight",1,7);
-		pieces[26] = new PieceData("w","bishop",2,7);
-		pieces[27] = new PieceData("w","queen",3,7);
-		pieces[28] = new PieceData("w","king",4,7);
-		pieces[29] = new PieceData("w","bishop",5,7);
-		pieces[30] = new PieceData("w","knight",6,7);
-		pieces[31] = new PieceData("w","rook",7,7);
+		pieces.put(Arrays.asList(0,7), new PieceData("w","rook",0,7));
+		pieces.put(Arrays.asList(1,7), new PieceData("w","knight",1,7));
+		pieces.put(Arrays.asList(2,7), new PieceData("w","bishop",2,7));
+		pieces.put(Arrays.asList(3,7), new PieceData("w","queen",3,7));
+		pieces.put(Arrays.asList(4,7), new PieceData("w","king",4,7));
+		pieces.put(Arrays.asList(5,7), new PieceData("w","bishop",5,7));
+		pieces.put(Arrays.asList(6,7), new PieceData("w","knight",6,7));
+		pieces.put(Arrays.asList(7,7), new PieceData("w","rook",7,7));
 		
-		
-		
+		List<Integer> p = Arrays.asList(0,1);
+
 		updateBoard();
 	}
 	
 	public void updateBoard() {
 		board = new PieceData[8][8];
 		
-		for(int i = 0; i < pieces.length;i++) {
-			board[pieces[i].getPosition().y][pieces[i].getPosition().x] = pieces[i];
+		for(PieceData p : pieces.values()) {
+			board[p.getPosition().y][p.getPosition().x] = p;
 		}
 		System.out.println(this.toString());
 	}
@@ -55,7 +60,19 @@ public class Game {
 		return true;
 	}
 	
-	public PieceData[] getPieces() {
+	public boolean checkCollision(PositionData p) {
+		for (List<Integer> key : pieces.keySet()) {
+			if(key.get(0) == p.x) {
+				if(key.get(1) == p.y) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	public HashMap<List<Integer>, PieceData> getPieces() {
 		return pieces;
 	}
 	
--- backend/PositionData.java
+++ backend/PositionData.java
@@ -2,11 +2,11 @@ package backend;
 
 import java.io.Serializable;
 
-public class Position implements Serializable {
+public class PositionData implements Serializable {
 	public int x;
 	public int y;
 	
-	public Position(int x, int y) {
+	public PositionData(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
