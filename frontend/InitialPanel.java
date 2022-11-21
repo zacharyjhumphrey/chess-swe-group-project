0df256a5ef19be43c8dc99245110a3541ebf8d9a
Zachary Humphrey <70534409+zacharyjhumphrey@users.noreply.github.com>
2022-11-21 09:01:29 -0600

    Update InitialPanel.java

--- frontend/InitialPanel.java
+++ frontend/InitialPanel.java
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-package chess;
-
-import java.awt.*;
-import javax.swing.*;
-
-public class InitialPanel extends JPanel
-{
-  // Constructor for the initial panel.
-  public InitialPanel(InitialControl ic)
-  {
-    // Create the information label.
-    JLabel label = new JLabel("Account Information", JLabel.CENTER);
-    
-    // Create the login button.
-    JButton loginButton = new JButton("Login");	
-    loginButton.addActionListener(ic);
-    JPanel loginButtonBuffer = new JPanel();
-    loginButtonBuffer.add(loginButton);
-    
-    // Create the create account button.
-    JButton createButton = new JButton("Create");
-    createButton.addActionListener(ic);
-    JPanel createButtonBuffer = new JPanel();
-    createButtonBuffer.add(createButton);
-
-    // Arrange the components in a grid.
-    JPanel grid = new JPanel(new GridLayout(3, 1, 5, 5));
-    grid.add(label);
-    grid.add(loginButtonBuffer);
-    grid.add(createButtonBuffer);
-    this.add(grid);
-  }
-}
-=======
 package frontend;
 
 import java.awt.*;
@@ -69,4 +34,3 @@ public class InitialPanel extends JPanel
   
   
 }
->>>>>>> 716601b (implemnted databse connectivity + panels/controllers)
